b4bd744fc0da52843258286f797aea9515ab6f01
Rami Rahikkala <rami.rahikkala@fluentia.fi>
2023-04-25 14:51:13 +0300

    Initial commit
    Signed-off-by: Rami Rahikkala <rami.rahikkala@fluentia.fi>

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const TemperatureVisualizer = () => {
+  const [temperatures, setTemperatures] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://ckslcpsx8d.execute-api.eu-central-1.amazonaws.com/v1/ruuvi-data');
+        setTemperatures(response.data.temperatures.latest);
+      } catch (error) {
+        console.error('Error fetching temperatures:', error);
+      }
+    };
+
+    fetchData(); // Fetch data immediately on component mount
+    const intervalId = setInterval(fetchData, 300000); // Update every 5 minutes (300000 ms)
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="temperature-visualizer">
+      <h1>Latest Temperatures</h1>
+      <ul>
+        {Object.entries(temperatures).map(([name, temp], index) => (
+          <li key={index}>{name}: {temp}°C</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <TemperatureVisualizer />
+    </div>
+  );
+}
+
+export default App;
