36ee83f6b40ceaff13e1e0d537eda68c3b9df110
Rami Rahikkala <rami.rahikkala@fluentia.fi>
2023-05-20 10:04:53 +0300

    Add different colours for lines
    Signed-off-by: Rami Rahikkala <rami.rahikkala@fluentia.fi>

--- src/TemperatureChart.js
+++ src/TemperatureChart.js
@@ -2,63 +2,56 @@ import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart, LineElement, TimeScale, LinearScale, PointElement, Tooltip, Legend } from 'chart.js';
 import 'chartjs-adapter-date-fns';
-import axios from 'axios';
 
 Chart.register(LineElement, TimeScale, LinearScale, PointElement, Tooltip, Legend);
 
+
 function TemperatureChart({ selectedTemperatures }) {
-    const [chartData, setChartData] = useState(null);
+  const [chartData, setChartData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const newChartData = { labels: [], datasets: [] };
+  // Array of colors for the chart lines
+  const colors = ['rgba(75,192,192,1)', 'rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
 
-            console.log("selectedTemperatures", selectedTemperatures); // Debugging point 1
+  useEffect(() => {
+    const fetchTemperatureData = async () => {
+      const datasets = await Promise.all(
+        Object.keys(selectedTemperatures).map(async (name, index) => {
+          if (selectedTemperatures[name]) {
+            const response = await fetch(`https://ckslcpsx8d.execute-api.eu-central-1.amazonaws.com/v1/ruuvi-data?measurementPoint=${name}&timeRange=24`);
+            const data = await response.json();
+            return {
+              label: name,
+              data: data.map(item => ({ x: new Date(item.datetime), y: parseFloat(item.temperature) })),
+              borderColor: colors[index % colors.length],
+              fill: false,
+            };
+          }
+          return null;
+        })
+      );
 
-            for (let key in selectedTemperatures) {
-                if (!selectedTemperatures[key]) continue;
+      setChartData({
+        datasets: datasets.filter(dataset => dataset !== null),
+      });
+    };
 
-                try {
-                    const response = await axios.get(`https://ckslcpsx8d.execute-api.eu-central-1.amazonaws.com/v1/ruuvi-data?measurementPoint=${key}&timeRange=24`);
-                    const data = response.data;
-                    const temperatures = data.map(item => parseFloat(item.temperature));
-                    const datetimes = data.map(item => new Date(item.datetime));
+    fetchTemperatureData();
+  }, [selectedTemperatures]);
 
-                    console.log(`Data for ${key}`, data); // Debugging point 2
-
-                    newChartData.labels = datetimes;
-                    newChartData.datasets.push({
-                        label: key,
-                        data: temperatures,
-                        borderColor: 'rgba(75,192,192,1)',
-                        fill: false,
-                    });
-
-                } catch (error) {
-                    console.error('Error fetching temperatures:', error);
-                }
-            }
-
-            setChartData(newChartData);
-        };
-
-        fetchData();
-    }, [selectedTemperatures]);
-
-    return (
-        <div>
-            {chartData &&
-                <Line
-                    data={chartData}
-                    options={{
-                        scales: {
-                            x: { type: 'time' },
-                        },
-                    }}
-                />
-            }
-        </div>
-    );
+  return (
+    <div>
+      {chartData &&
+        <Line 
+          data={chartData} 
+          options={{ 
+            scales: {
+              x: { type: 'time' },
+            },
+          }} 
+        />
+      }
+    </div>
+  );
 }
 
 export default TemperatureChart;
